3874715736a6401f3c1fae68fb74ea27101675af
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-11-04 19:21:59 -0400

    Password will display in text area, old password needs to be reset

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Assignment code here
-
-// Push random letters, numbers, character codes into an array:
-// Have an empty array and push everything into it separately?
-// Take everything and use REST operator to turn them into an array then SPREAD operator to output them separately?
-
-//STEP 1 - Create arrays for all characters and merge them into a single array
-
 //Adds all special characters into a single array with each character having its own position
 const specialChar = [..."!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
 // Adds all lower case and upper case letters into single strings
@@ -41,24 +33,9 @@ const numS = [...numbers, ...specialChar];
 // ---------------------------------------------ASSIGNED VARIABLES---------------------------------------
 // Asks for password length
 let passwordLength;
-const askForLength = function () {
-  passwordLength = Number(
-    prompt(`Please choose the length of the password you would like
-    Min: 8 characters long
-    Max: 128 characters long`)
-  );
-
-  // TODO: Study regex to add in if what is inputed isnt a number
-  if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
-    alert("Not valid");
-    askForLength();
-  }
-};
-askForLength();
 
 // STEP 3 - Generate random character passwords
 //FIXME: These will sometimes give undefined, check why
-// TODO: Add a few more functions, missing some like high-num
 // Each function will generate a password depending on which type of characters the user desires
 
 // All characters
@@ -124,6 +101,7 @@ const generatePassword = function (generator) {
     newPassWord.push(generator());
   }
   console.log(newPassWord);
+  return newPassWord;
 };
 
 // generatePassword(genNumSpecial);
@@ -136,149 +114,151 @@ let passwordLowerCase;
 let passwordSpecialChars;
 let password;
 
-const ask = function () {
-  passwordUpperCase = confirm(
-    "Would you like to use uppercase characters in your password?"
-  );
-  passwordLowerCase = confirm(
-    "Would you like to use lowercase characters in your password?"
-  );
-  passwordNumbers = confirm(
-    "Would you like to use numerical values in your password?"
-  );
-  passwordSpecialChars = confirm(
-    "Would you like to use special characters in your password?"
-  );
-
-  // Chooses password generator based on user input
-  if (
-    // Just uppercase
-    passwordUpperCase &&
-    !passwordLowerCase &&
-    !passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(generateUpperChar);
-  } else if (
-    // Just lower case
-    !passwordUpperCase &&
-    passwordLowerCase &&
-    !passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(generateLowerChar);
-  } else if (
-    // Just numbers
-    !passwordUpperCase &&
-    !passwordLowerCase &&
-    passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(generateNumbers);
-  } else if (
-    // Just special characters
-    !passwordUpperCase &&
-    !passwordLowerCase &&
-    !passwordNumbers &&
-    passwordSpecialChars
-  ) {
-    generatePassword(generateSpecialChar);
-  } else if (
-    // All characters
-    passwordUpperCase &&
-    passwordLowerCase &&
-    passwordNumbers &&
-    passwordSpecialChars
-  ) {
-    generatePassword(generateAllChar);
-  } else if (
-    // Uppercase and lowercase
-    passwordUpperCase &&
-    passwordLowerCase &&
-    !passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(genHighLow);
-  } else if (
-    // Uppercase and lowercase and numbers
-    passwordUpperCase &&
-    passwordLowerCase &&
-    passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(genHighLowNum);
-  } else if (
-    // uppercase and numbers
-    passwordUpperCase &&
-    !passwordLowerCase &&
-    passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(genHighNum);
-  } else if (
-    // uppercase and special characters
-    passwordUpperCase &&
-    !passwordLowerCase &&
-    !passwordNumbers &&
-    passwordSpecialChars
-  ) {
-    generatePassword(genHighSpecial);
-  } else if (
-    // lowercase and number
-    !passwordUpperCase &&
-    passwordLowerCase &&
-    passwordNumbers &&
-    !passwordSpecialChars
-  ) {
-    generatePassword(genlowNum);
-  } else if (
-    // lowercase and special characters
-    !passwordUpperCase &&
-    passwordLowerCase &&
-    !passwordNumbers &&
-    passwordSpecialChars
-  ) {
-    generatePassword(genLowSpecial);
-  } else if (
-    // numbers and special characters
-    !passwordUpperCase &&
-    !passwordLowerCase &&
-    passwordNumbers &&
-    passwordSpecialChars
-  ) {
-    generatePassword(genNumSpecial);
-  }
-};
-ask();
-// --------------------------------------Experimental------------------------------------------
-// const choice = [
-//   "high",
-//   "low",
-//   "num",
-//   "special",
-//   "high-low",
-//   "high-low-num",
-//   "allChar",
-//   "high-num",
-//   "high-special",
-//   "low-num",
-//   "low-special",
-//   "num-special",
-// ];
-
-// --------------------------------------Experimental------------------------------------------
+// TODO: Add choice for if user doesn't want a password generated anymore
 
 // --------------------------------------DOM------------------------------------------
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// function reset() {}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePasswordssss();
+  const askForLength = function () {
+    passwordLength = Number(
+      prompt(`Please choose the length of the password you would like
+      Min: 8 characters long
+      Max: 128 characters long`)
+    );
+
+    // TODO: Study regex to add in if what is inputed isnt a number
+    if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
+      alert("Not valid");
+      askForLength();
+    }
+  };
+  askForLength();
+
+  const ask = function () {
+    passwordUpperCase = confirm(
+      "Would you like to use uppercase characters in your password?"
+    );
+    passwordLowerCase = confirm(
+      "Would you like to use lowercase characters in your password?"
+    );
+    passwordNumbers = confirm(
+      "Would you like to use numerical values in your password?"
+    );
+    passwordSpecialChars = confirm(
+      "Would you like to use special characters in your password?"
+    );
+
+    // Chooses password generator based on user input
+    if (
+      // Just uppercase
+      passwordUpperCase &&
+      !passwordLowerCase &&
+      !passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(generateUpperChar);
+    } else if (
+      // Just lower case
+      !passwordUpperCase &&
+      passwordLowerCase &&
+      !passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(generateLowerChar);
+    } else if (
+      // Just numbers
+      !passwordUpperCase &&
+      !passwordLowerCase &&
+      passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(generateNumbers);
+    } else if (
+      // Just special characters
+      !passwordUpperCase &&
+      !passwordLowerCase &&
+      !passwordNumbers &&
+      passwordSpecialChars
+    ) {
+      generatePassword(generateSpecialChar);
+    } else if (
+      // All characters
+      passwordUpperCase &&
+      passwordLowerCase &&
+      passwordNumbers &&
+      passwordSpecialChars
+    ) {
+      generatePassword(generateAllChar);
+    } else if (
+      // Uppercase and lowercase
+      passwordUpperCase &&
+      passwordLowerCase &&
+      !passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(genHighLow);
+    } else if (
+      // Uppercase and lowercase and numbers
+      passwordUpperCase &&
+      passwordLowerCase &&
+      passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(genHighLowNum);
+    } else if (
+      // uppercase and numbers
+      passwordUpperCase &&
+      !passwordLowerCase &&
+      passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(genHighNum);
+    } else if (
+      // uppercase and special characters
+      passwordUpperCase &&
+      !passwordLowerCase &&
+      !passwordNumbers &&
+      passwordSpecialChars
+    ) {
+      generatePassword(genHighSpecial);
+    } else if (
+      // lowercase and number
+      !passwordUpperCase &&
+      passwordLowerCase &&
+      passwordNumbers &&
+      !passwordSpecialChars
+    ) {
+      generatePassword(genlowNum);
+    } else if (
+      // lowercase and special characters
+      !passwordUpperCase &&
+      passwordLowerCase &&
+      !passwordNumbers &&
+      passwordSpecialChars
+    ) {
+      generatePassword(genLowSpecial);
+    } else if (
+      // numbers and special characters
+      !passwordUpperCase &&
+      !passwordLowerCase &&
+      passwordNumbers &&
+      passwordSpecialChars
+    ) {
+      generatePassword(genNumSpecial);
+    }
+  };
+  ask();
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newPassWord;
 }
 
 // Add event listener to generate button
